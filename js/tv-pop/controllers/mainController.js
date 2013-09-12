d6b40d1f9dc5358cf9b67feb36dfa7c72adb599f
tylerbisset <tyler@tylerbisset.com>
2013-09-11 20:02:07 -0400

    Style tiles and css update
    Added list and detail style tile. Implemented styles to framework.
    Commented main controller.

--- js/tv-pop/controllers/mainController.js
+++ js/tv-pop/controllers/mainController.js
@@ -5,20 +5,19 @@ app.controller("mainController", function($scope, $http){
     $scope.filterText = null;
     
     $scope.init = function() {
-        //API requires a start date
+        //API requires start date
         var today = new Date();
-        //Create the date string and ensure leading zeros if required
+        //date string
         var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
         $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
-            //As we are getting our data from an external source, we need to format the data so we can use it to our desired affect
-            //For each day get all the episodes
+            //format data for each day get all the episodes
             angular.forEach(data, function(value, index){
-                //The API stores the full date separately from each episode. Save it so we can use it later
+                //API stores full date separately from each episode. Save it for use later
                 var date = value.date;
-                //For each episodes add it to the results array
+                //for each episodes add it to the results array
                 angular.forEach(value.episodes, function(tvshow, index){
-                    //Create a date string from the timestamp so we can filter on it based on user text input
-                    tvshow.date = date; //Attach the full date to each episode
+                    //create a date string from timestamp to filter based on input
+                    tvshow.date = date; //attach date to each episode
                     $scope.results.push(tvshow);
                 });
             });
